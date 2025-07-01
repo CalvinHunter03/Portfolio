d8698d3de138741f2c313a1ce5c43882173ca59e
Calvin Hunter <calvinhunter03@gmail.com>
2025-07-01 14:32:20 -0700

    added some color and created colums for about me and projects

--- homepage.js
+++ homepage.js
@@ -10,7 +10,17 @@ top_bar.appendChild(nameHeading);
 // INTRO
 let introP = document.createElement("p");
 introP.innerHTML = "Hello, my name is Calvin Hunter!";
+
+let introTitle = document.createElement("h1");
+introTitle.innerHTML = "About Me";
+introTitle.id = "intro_title";
+
+intro.appendChild(introTitle);
 intro.appendChild(introP);
 
 //PROJECTS
+let souperHero = document.getElementById("souper_hero");
+let souperTitle = document.createElement("h1");
+souperTitle.innerHTML = "Souper Hero";
+souperHero.appendChild(souperTitle);
 
