eab1fdd6fe195db236bb31a02e330dae89d2c243
Calvin Hunter <70008107+CalvinHunter03@users.noreply.github.com>
2025-06-30 21:49:51 -0700

    added some more to the page

---
+++ homepage.js
@@ -0,0 +1,16 @@
+let top_bar = document.getElementById("top_bar");
+let intro = document.getElementById("intro");
+let projects = document.getElementById("projects");
+
+//TOP BAR
+let nameHeading = document.createElement("h1");
+nameHeading.innerHTML = "Calvin Hunter";
+top_bar.appendChild(nameHeading);
+
+// INTRO
+let introP = document.createElement("p");
+introP.innerHTML = "Hello, my name is Calvin Hunter!";
+intro.appendChild(introP);
+
+//PROJECTS
+
