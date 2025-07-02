2598cb9a2aa7483c8f413697e6d10bb285782776
Calvin Hunter <calvinhunter03@gmail.com>
2025-07-01 18:46:49 -0700

    messing around with colors
    trying to format as well

--- homepage.js
+++ homepage.js
@@ -2,6 +2,13 @@ let top_bar = document.getElementById("top_bar");
 let intro = document.getElementById("intro");
 let projects = document.getElementById("projects");
 
+function addList(appender, theList){
+    for(let element of theList){
+        appender.appendChild(element);
+    }
+}
+
+
 //TOP BAR
 let nameHeading = document.createElement("h1");
 nameHeading.innerHTML = "Calvin Hunter";
@@ -20,7 +27,24 @@ intro.appendChild(introP);
 
 //PROJECTS
 let souperHero = document.getElementById("souper_hero");
+
 let souperTitle = document.createElement("h1");
-souperTitle.innerHTML = "Souper Hero";
-souperHero.appendChild(souperTitle);
+souperTitle.id = "souper_title";
+souperTitle.innerHTML = "<u>Souper Hero</u>";
+
+let souperDescOne = document.createElement("li");
+souperDescOne.innerHTML = "Level Design";
+souperDescOne.classList.add('souper_desc');
+
+let souperDescTwo = document.createElement("li");
+souperDescTwo.innerHTML = "Combat functionality";
+souperDescTwo.classList.add('souper_desc');
+
+let souperDescThree = document.createElement("li");
+souperDescThree.innerHTML = "Fixing combat bugs";
+souperDescThree.classList.add('souper_desc');
+
+let souperList = [souperTitle, souperDescOne, souperDescTwo, souperDescThree];
+
+addList(souperHero, souperList);
 
